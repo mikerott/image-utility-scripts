6cdf4f3dd1a842fa105ff1f6e27d6ada60abec08
Mike Rheinheimer <mrheinheimer@ohmyhead.com>
2017-04-11 23:16:04 -0500

    lots of improvements

--- convert_samples.sh
+++
@@ -1,8 +0,0 @@
-list=`ls IMG_*`
-for line in ${list}
-do
-	filename=`echo ${line} | cut -f1 -d'.'`
-	# widescreen:  example starting with 1600x1200, crop to bottom 800px scale to 720 height, preserving aspect
-	nice -19 convert ${line} -crop 1600x900+0+0 -scale x720 -unsharp 0.3x2+1.5 -quality 80 ${filename}_tovid.jpg
-	#convert ${line} -scale x720 -unsharp 0.3x2+1.5 -quality 80 ${filename}_tovid.jpg
-done
--- create-timelapse.sh
+++ create-timelapse.sh
@@ -1,52 +1,151 @@
 #!/bin/bash
 
+# This is a work in progress.
+# prompt the end user for input, and validate it until they succeed:
+# http://alvinalexander.com/linux-unix/shell-script-how-prompt-read-user-input-bash
+
 me=`basename "$0"`
 
-function timelapse() {
+function scaled_timelapse() {
+    command -v nice >/dev/null 2>&1 || { echo >&2 "'nice' is not installed; aborting."; exit 1; }
+    command -v mencoder >/dev/null 2>&1 || { echo >&2 "'mencoder' is not installed; aborting."; exit 1; }
+
+    re='^10|15|30|60+$'
+    if ! [[ $1 =~ $re ]] ; then
+       echo "error: First argument must be a framerate set to 10, 15, 30, or 60" >&2; exit 1
+    fi
+
+    re='^[a-zA-Z0-9]+$'
+    if ! [[ $2 =~ $re ]] ; then
+       echo "error: Second argument must be the desired filename consisting of at least one alphanumeric char" >&2; exit 1
+    fi
+
+    re='^[0-9]+$'
+    if ! [[ $3 =~ $re ]] ; then
+       echo "error: Third argument is the desired scaled maximum width and must be between 320 and 1920" >&2; exit 1
+    fi
+
+    if [ $3 -ge 1921 ] || [ $3 -le 319 ] ; then
+       echo "error: Third argument is the desired scaled maximum width and must be between 320 and 1920" >&2; exit 1
+    fi
+
+    if ! [[ $4 =~ $re ]] ; then
+       echo "error: Fourth argument is the desired scaled maximum height and must be between 240 and 1080" >&2; exit 1
+    fi
+
+    if [ $4 -ge 1081 ] || [ $4 -le 239 ] ; then
+       echo "error: Fourth argument is the desired scaled maximum height and must be between 240 and 1080" >&2; exit 1
+    fi
+
     framerate="$1"
     filename="$2"
     resolution_width="$3"
     resolution_height="$4"
-    ls -1tr > timelapse_list.txt
-    mencoder -nosound -ovc lavc -lavcopts \
+    nice -19 mencoder -nosound -ovc lavc -lavcopts \
         vcodec=mpeg4:mbd=2:trell:autoaspect:vqscale=3 \
         -vf scale=$resolution_width:$resolution_height -mf type=jpeg:fps=$framerate \
-        mf://@timelapse_list.txt -o $filename
+        mf://*.jpg -o $filename.mp4
+}
+
+function timelapse() {
+    command -v nice >/dev/null 2>&1 || { echo >&2 "'nice' is not installed; aborting."; exit 1; }
+    command -v mencoder >/dev/null 2>&1 || { echo >&2 "'mencoder' is not installed; aborting."; exit 1; }
+
+    re='^10|15|30|60+$'
+    if ! [[ $1 =~ $re ]] ; then
+       echo "error: First argument must be a framerate set to 10, 15, 30, or 60" >&2; exit 1
+    fi
+
+    re='^[a-zA-Z0-9]+$'
+    if ! [[ $2 =~ $re ]] ; then
+       echo "error: Second argument must be the desired filename consisting of at least one alphanumeric char" >&2; exit 1
+    fi
+
+    framerate="$1"
+    filename="$2"
+    nice -19 mencoder -nosound -ovc lavc -lavcopts \
+        vcodec=msmpeg4v2 \
+        -mf type=jpeg:fps=$framerate \
+        mf://*.jpg -o $filename.mp4
 }
 
 function superview() {
-    for i in *.MP4;
+    command -v ffmpeg >/dev/null 2>&1 || { echo >&2 "'ffmpeg' is not installed; aborting."; exit 1; }
+
+    for i in *.mp4;
         do name=`echo $i | cut -d'.' -f1`+S;
         echo $name;
-        ffmpeg -i $i -sameq -vcodec mpeg4 -acodec ac3 -aspect 16:9 -strict experimental $name.MP4
+        ffmpeg -i $i -sameq -vcodec mpeg4 -acodec ac3 -aspect 16:9 -strict experimental $name.mp4
     done
 }
 
-function fisheye(){
-    mogrify -distort barrel "0 0 -0.3" *.JPG
+function fisheye() {
+    command -v mogrify >/dev/null 2>&1 || { echo >&2 "'mogrify' is not installed; aborting."; exit 1; }
+
+    mogrify -distort barrel "0 0 -0.3" *.jpg
 }
 
 function convert(){
-for i in *.MP4;
-    do name=`echo $i | cut -d'.' -f1`;
-    echo $name;
-    ffmpeg -i $i -sameq -vcodec mpeg4 $name.mov;
-done
+    command -v ffmpeg >/dev/null 2>&1 || { echo >&2 "'ffmpeg' is not installed; aborting."; exit 1; }
+
+    for i in *.mp4;
+        do name=`echo $i | cut -d'.' -f1`;
+        echo $name;
+        ffmpeg -i $i -sameq -vcodec mpeg4 $name.mov;
+    done
+}
+
+function resize() {
+    command -v nice >/dev/null 2>&1 || { echo >&2 "'nice' is not installed; aborting."; exit 1; }
+    command -v convert >/dev/null 2>&1 || { echo >&2 "'convert' is not installed; aborting."; exit 1; }
+
+    list=`ls *.jpg`
+    for line in ${list}
+    do
+  	    filename=`echo ${line} | cut -f1 -d'.'`
+  	    # widescreen:  example starting with 1600x1200, crop to bottom 800px scale to 720 height, preserving aspect
+  	    nice -19 convert ${line} -crop 1600x900+0+0 -scale x720 -unsharp 0.3x2+1.5 -quality 80 ${filename}_tovid.jpg
+  	    #convert ${line} -scale x720 -unsharp 0.3x2+1.5 -quality 80 ${filename}_tovid.jpg
+    done
 }
 
 function trim(){
+    command -v ffmpeg >/dev/null 2>&1 || { echo >&2 "'ffmpeg' is not installed; aborting."; exit 1; }
+
     ffmpeg -i $1 -ss $3 -t $4 -sameq -vcodec libx264 $2
 }
 
 function help() {
     me=`basename "$0"`
-    printf "Create Time Lapse Script\nUsage:\n\n- $me timelapse [fps] [outfilename] [res width] [res height]\nMakes a timelapse with images in the current folder.\nExample: $me timelapse 30 my-timelapse.mp4 1920 1080\n\n- $me superview\nApplies SuperView to all videos in the current dir\n\n- $me fisheye\nFixes barrel distortion to all images in the current folder\n\n- $me convert\nConverts all MP4 videos to MPEG4 MOV videos for easy editing\n\n- $me trim [input video] [output video] [HH:MM:SS start] [HH:MM:SS stop]\nTrims a video, use this to trim a slow motion video!\nExample: $me trim GOPR0553.MP4 Trimmed.mp4 00:05:04 00:07:43\n\n"
+    printf "Create Time Lapse Script\n"
+    printf "Usage:\n"
+    printf "\n"
+    printf "$me timelapse [fps] [outfilename]\n"
+    printf "Makes a timelapse with all images (*.jpg) in the current folder.\n"
+    printf "Example: $me timelapse 30 timelapse.mp4\n"
+    printf "\n"
+    printf "$me scaled_timelapse [fps] [outfilename] [res width] [res height]\n"
+    printf "Makes a timelapse with all images (*.jpg) in the current folder.\n"
+    printf "Example: $me timelapse 30 scaledtimelapse.mp4 1920 1080\n"
+    printf "\n"
+    printf "$me superview\n"
+    printf "Applies SuperView to all videos (*.mp4) in the current dir\n"
+    printf "\n"
+    printf "$me fisheye\n"
+    printf "Fixes barrel distortion to all images (*.jpg) in the current folder\n"
+    printf "\n"
+    printf "$me convert\n"
+    printf "Converts all MP4 (*.mp4) videos to MPEG4 MOV videos for easy editing\n"
+    printf "\n"
+    printf "$me resize\n"
+    printf "Crops, resizes, and sharpens all images (*.jpg) in the current folder to the hard-coded values, copying the output to new file suffixed with _tovid\n"
+    printf "\n"
+    printf "$me trim [input video] [output video] [HH:MM:SS start] [HH:MM:SS stop]\n"
+    printf "Trims a video, use this to trim a slow motion video!\n"
+    printf "Example: $me trim original.mp4 trimmed.mp4 00:05:04 00:07:43\n"
+    printf "\n"
 }
 
 echo "Create Time Lapse Script"
 echo "To see a list of commands and syntax available run: $me help"
-echo "Checking dependencies..."
-hash ffmpeg 2> /dev/null || { echo >&2 "ffmpeg ..... Not installed!";}
-hash mogrify 2> /dev/null || { echo >&2 "mogrify ..... Not installed!";}
-hash mencoder 2> /dev/null || { echo >&2 "mencoder ..... Not installed!";}
 $@
